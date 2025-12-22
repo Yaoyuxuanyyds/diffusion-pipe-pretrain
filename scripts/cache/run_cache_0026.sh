337ee78ed34f305d278fc4f4353046325269bc18
Yaoyuxuanyyds <1282557919@qq.com>
2025-12-22 07:20:51 +0000

    update

--- scripts/cache/run_cache_0026.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-source /inspire/hdd/project/chineseculture/public/yuxuan/miniconda3/etc/profile.d/conda.sh
-conda activate diffusion-pipe
-cd /inspire/hdd/project/chineseculture/public/yuxuan/diffusion-pipe
-
-NCCL_P2P_DISABLE="0" NCCL_IB_DISABLE="0" deepspeed --num_gpus=8 train.py --deepspeed --config /inspire/hdd/project/chineseculture/public/yuxuan/diffusion-pipe/settings/cache/train_configs/sd3_cache_0026.toml --cache_only --regenerate_cache
